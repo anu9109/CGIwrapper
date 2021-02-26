7000a9dd9e7bbafa981a7cc456cc0186b7b83ee9
Anu Amallraja <anu9109@gmail.com>
2021-02-25 19:27:20 -0600

    added roxygen documentation

--- R/CGIwrapper.R
+++ R/CGIwrapper.R
@@ -12,23 +12,19 @@ shhh(library(fs))
 
 # parse input arguments
 option_list <- list(
-  make_option(c("-e", "--email"), action = "store", type = "character",
-              help = "Email address authorized to access the CGI API"),
-  make_option(c("-t", "--token"), action = "store", type = "character",
-              help = "API token obtained from the CGI website"),
   make_option(c("-a", "--auth"), action = "store", type = "character", default = ".auth.json",
-              help = "Hidden file containing authentication details (recommended)"), 
+              help = "Hidden file containing authentication details (recommended)"),
   make_option(c("-i", "--id"), action = "store", type = "character",
               help = "Provide an ID for the CGI job (required)"),
-  make_option(c("-m", "--mut"), action = "store", type = "character", default = NULL, 
+  make_option(c("-m", "--mut"), action = "store", type = "character", default = NULL,
               help = "Provide input mutations as file according to CGI format (.tsv) (optional)
               Atleast one input file is required"),
-  make_option(c("-c", "--cna"), action = "store", type = "character", default = NULL, 
+  make_option(c("-c", "--cna"), action = "store", type = "character", default = NULL,
               help = "Provide input copy numbers as file according to CGI format (.tsv) (optional)
               Atleast one input file is required"),
-  make_option(c("-f",  "--fus"), action = "store", type = "character", default = NULL, 
+  make_option(c("-f",  "--fus"), action = "store", type = "character", default = NULL,
               help = "Provide input fusions as file according to CGI format (.tsv) (optional)
-              Atleast one input file is required"), 
+              Atleast one input file is required"),
   make_option(c("-y", "--type"), action = "store", type = "character", default = "CANCER",
               help = "Provide cancer type for the sample (optional, default %default)"),
   make_option(c("-o", "--output"), action = "store", type = "character",
@@ -42,33 +38,29 @@ opt <- parse_args(OptionParser(option_list = option_list,
                                description = "R wrapper for the CGI API to submit, download and delete jobs"))
 
 
-## read in email and api key from .auth.json OR arguments
-## args take precedence if both args and auth file are present
+#' Check Authentication
+#'
+#' This function reads in email and api key from the provided .auth.json file
+#'
+#' @param secret secrets JSON file containing the email address and API token
+cgi_checkAuth <- function(secret) {
 
-cgi_checkAuth <- function() {
-  # check for command line args first 
   message("Checking authentication details.")
-  if (!any(c(is.null(opt$email), is.null(opt$token)))) {
-    email <- opt$email
-    token <- opt$token
-  }
-  else {
-    # check for .auth.json file next
-    secret <- if_else(file.exists(here(".auth.json")), here(".auth.json"), opt$auth)
-    if (file.exists(secret)) {
-      s <- read_json(secret)
-      email <- s$email
-      token <- s$token
-    }
-    else
-      abort("No authentication information provided.")
-  }
 
-    sec <- list("email" = email, "token" = token)
+  # check for .auth.json file
+  if (file.exists(secret)) {
+    s <- read_json(secret)
+    email <- s$email
+    token <- s$token
+  }
+  else
+    abort("No authentication information provided.")
+
+  sec <- list("email" = email, "token" = token)
   return(sec)
 }
 
-auth <- cgi_checkAuth()
+auth <- cgi_checkAuth(secret)
 
 # storing arguments in variables for further use
 cgi_email <- auth$email
@@ -81,7 +73,13 @@ cancer_type <- opt$type
 outpath <- opt$output
 
 
-## check to make sure atleast one input file has been provided
+#' Check Input Files
+#'
+#' This function checks that atleast one input file has been provided.
+#'
+#' @param mut input mutations as file according to CGI format (.tsv)
+#' @param cna input copy number variants as file according to CGI format (.tsv)
+#' @param fus input fusions as file according to CGI format (.tsv)
 cgi_checkInput <- function(mut, cna, fus){
   message("Checking to make sure atleast one input file is provided.")
   if (!any(c(!is.null(mut), !is.null(cna), !is.null(fus)))) {
@@ -90,7 +88,12 @@ cgi_checkInput <- function(mut, cna, fus){
 }
 
 
-## check to see if authentication info/api service are working as intended
+#' Get API Status
+#'
+#' This function checks to see if authentication info/api service are working as intended.
+#'
+#' @param api_url URL of the CGI API
+#' @param header authentication information as a header
 cgi_checkApiAuth <- function(api_url, header) {
   apicheck <- GET(url = api_url,header) %>%
     status_code() %>%
@@ -106,7 +109,17 @@ cgi_checkService <- function(api_url, header) {
 }
 
 
-# submit job 
+#' Submit Job
+#'
+#' This function submits a job to the CGI server.
+#'
+#' @param api_url URL of the CGI API
+#' @param header authentication information as a header
+#' @param mut input mutations as file according to CGI format (.tsv)
+#' @param cna input copy number variants as file according to CGI format (.tsv)
+#' @param fus input fusions as file according to CGI format (.tsv)
+#' @param cancer_type valid cancer type (default: CANCER)
+#' @param id ID for the CGI job
 cgi_submitJob <- function(api_url, header, mut, cna, fus, cancer_type, id){
   r <- POST(url = api_url,
             header,
@@ -123,7 +136,10 @@ cgi_submitJob <- function(api_url, header, mut, cna, fus, cancer_type, id){
 }
 
 
-# get job status
+#' Get Job Status
+#'
+#' @param job_url URL of the specific job submitted
+#' @param header authentication information as a header
 cgi_getJobStatus <- function(job_url, header){
   status <- GET(url = job_url,
                 header,
@@ -133,22 +149,27 @@ cgi_getJobStatus <- function(job_url, header){
 }
 
 
-# download results
+#' Download Results
+#'
+#' @param job_url URL of the specific job submitted
+#' @param header authentication information as a header
+#' @param outpath full path of output directory
+#' @param id ID for the CGI job
 cgi_downloadJob <- function(job_url, header, outpath, id){
-  
+
   # outpath manipulation to make sure results are downloaded correctly when script is run standalone or through nf
   # if outpath is provided then add a trailing backslash to acommodate for paste0 in the following write_delim
   if (outpath != "nf") {
     outpath <- path_tidy(outpath)
     outpath <- paste0(outpath, "/")
-    
+
     if (!dir_exists(outpath)) {
       dir_create(outpath)
-    } 
+    }
   } else {
     outpath <- "" # this is for the following paste0 to work correctly with nf pipeline
   }
-  
+
   outfile <- paste0(outpath, id, ".zip")
   download <- GET(url = job_url,
              header,
@@ -158,7 +179,16 @@ cgi_downloadJob <- function(job_url, header, outpath, id){
   return(download)
 }
 
-# if job has successfully finished (or errored out) then download, else wait 15s and check status again
+
+#' Wait Until Job Finishes
+#'
+#' This function checks if job has successfully finished (or errored out) then downloads the output,
+#' else waits for 15s and checks status again.
+#'
+#' @param job_url URL of the specific job submitted
+#' @param header authentication information as a header
+#' @param outpath full path of output directory
+#' @param id ID for the CGI job
 cgi_wait <- function(job_url, header, outpath, id) {
   while(!(cgi_getJobStatus(job_url, header) %in% c("Done", "Error"))) {
     message("Waiting for job to complete, checking status again in 15s..")
@@ -169,7 +199,13 @@ cgi_wait <- function(job_url, header, outpath, id) {
 }
 
 
-# delete job 
+
+#' Delete Job
+#'
+#' This function deletes a job from the CGI server given a job ID.
+#'
+#' @param job_url URL of the specific job submitted
+#' @param header authentication information as a header
 cgi_deleteJob <- function(job_url, header){
   del <- DELETE(url = job_url,
                 header)
@@ -179,6 +215,24 @@ cgi_deleteJob <- function(job_url, header){
 
 
 ## run everything at once - submit job + check status + download results + delete job
+#' Run All
+#'
+#' This function does the folloowing:
+#' (i) checks whether the given authentication information works on the CGI API.
+#' (ii) verifies whether atleast one input has been provided
+#' (iii) submits a job to the CGI API
+#' (iv) downloads the results zip file
+#' (v) deletes the job from the server
+#'
+#' @param cgi_email secrets JSON file containing the email address and API token
+#' @param cgi_token secrets JSON file containing the email address and API token
+#' @param mut input mutations as file according to CGI format (.tsv)
+#' @param cna input copy number variants as file according to CGI format (.tsv)
+#' @param fus input fusions as file according to CGI format (.tsv)
+#' @param cancer_type valid cancer type (default: CANCER)
+#' @param id ID for the CGI job
+#' @param outpath full path of output directory
+#' @export
 cgi_run <- function(cgi_email, cgi_token, mut, cna, fus, cancer_type, id, outpath){
   api_url <- "https://www.cancergenomeinterpreter.org/api/v1"
   header <- add_headers(Authorization = paste0(cgi_email, " ", cgi_token))
@@ -190,7 +244,7 @@ cgi_run <- function(cgi_email, cgi_token, mut, cna, fus, cancer_type, id, outpat
   cgi_deleteJob(a$job_url, header)
 }
 
-## call main function 
-cgi_run(cgi_email, cgi_token, mut, cna, fus, cancer_type, id, outpath)
+## call main function
+#cgi_run(cgi_email, cgi_token, mut, cna, fus, cancer_type, id, outpath)
 
 
